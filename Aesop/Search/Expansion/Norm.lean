8fb8093f9d37dbad1f9616c71ade8a1f34ae3d4a
vnh1806 <vunhathoang96@gmail.com>
2025-04-01 14:58:53 +0200

    last change

--- Aesop/Search/Expansion/Norm.lean
+++ Aesop/Search/Expansion/Norm.lean
@@ -381,7 +381,27 @@ def simp (mvars : Std.HashSet MVarId) : NormStep
     return optNormRuleResultToNormSeqResult r
 
 end NormStep
+-- Aesop branch: rpinf-precomp
+-- make new branch
+-- git cherry-pick <your commit>
+-- discard changes with git restore before rebranching
+-- upstream/rpinf-precomp in case
+-- import precomp rpinf
 
+
+  /-def NormStep.reduceAllInGoal : NormStep
+  | goal, _, _ => do
+      let (newGoal, time) ← time (Aesop.reduceAllInGoal goal false false false)
+      trace[debug] "Execution time for reduceAllInGoal: {time.printAsMillis}"
+      modifyCurrentStats λ stats => {stats with reduceAllInGoal := stats.reduceAllInGoal + time}
+      if newGoal == goal then
+        return .unchanged
+      else
+        return .changed newGoal #[]
+--
+-/
+
+--NVU
 partial def normalizeGoalMVar (goal : MVarId)
     (mvars : UnorderedArraySet MVarId) : NormM NormSeqResult := do
   let mvarsHashSet := .ofArray mvars.toArray
